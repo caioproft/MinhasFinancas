f3bf8e9c2b668d570b9576ddc6925c6ec99c2e15
Caio Proft <caio_proft@hotmail.com>
2019-12-09 23:34:02 -0300

    Adição da classe Service e método para validar se já existe um e-mail cadastrado no banco de dados

--- src/main/java/com/invillia/minhasFinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/invillia/minhasFinancas/service/impl/UsuarioServiceImpl.java
@@ -1,13 +1,18 @@
 package com.invillia.minhasFinancas.service.impl;
 
+import com.invillia.minhasFinancas.exception.RegraNegocioException;
 import com.invillia.minhasFinancas.model.entity.Usuario;
 import com.invillia.minhasFinancas.repository.UsuarioRepository;
 import com.invillia.minhasFinancas.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UsuarioServiceImpl implements UsuarioService {
 
     private UsuarioRepository repository;
 
+    @Autowired
     public UsuarioServiceImpl(UsuarioRepository repository) {
         this.repository = repository;
     }
@@ -24,6 +29,9 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Override
     public void validarEmail(String email) {
-
+        boolean existe = repository.existsByEmail(email);
+        if(existe){
+            throw new RegraNegocioException("Já existe um usuário cadastrado com esse e-mail");
+        }
     }
 }
