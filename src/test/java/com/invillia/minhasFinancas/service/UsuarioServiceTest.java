c5191c4f6c07fd9958c956f92724e2427e8eb9e8
Caio Proft <caio_proft@hotmail.com>
2019-12-12 21:38:14 -0300

    Adição dos testes de integração na classe UsuarioService para validar e-mails já cadastrados

---
+++ src/test/java/com/invillia/minhasFinancas/service/UsuarioServiceTest.java
@@ -0,0 +1,49 @@
+package com.invillia.minhasFinancas.service;
+
+
+import com.invillia.minhasFinancas.exception.RegraNegocioException;
+import com.invillia.minhasFinancas.model.entity.Usuario;
+import com.invillia.minhasFinancas.repository.UsuarioRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class UsuarioServiceTest {
+
+    @Autowired
+    UsuarioService service;
+
+    @Autowired
+    UsuarioRepository repository;
+
+    @Test
+    public void deveValidarEmail() {
+            //cenário
+            repository.deleteAll();
+
+            //ação
+            service.validarEmail("usuario@email.com");
+    }
+
+
+    @Test
+    public void deveLançarErroAoValidarEmailQuandoExisteEmailCadastrado() {
+        Assertions.assertThrows(RegraNegocioException.class, () -> {
+            // cenário
+            Usuario usuario = Usuario.builder().nome("Usuário").email("usuario@email.com").build();
+            repository.save(usuario);
+
+            // ação
+            service.validarEmail("usuario@email.com");
+        });
+    }
+
+
+}
